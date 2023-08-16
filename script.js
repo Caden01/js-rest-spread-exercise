d16934ef0143988fed8d512f96a24ebd0469840c
Caden01 <caden.carlson01@gmail.com>
2023-08-16 10:15:10 -0700

    added a funciton that takes two objects and puts all the keys and values of the two into one new object and made a test to check if it works

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ function findMin(...args) {
 // mergeObjects
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 
+function mergeObjects(obj1, obj2) {
+  return { ...obj1, ...obj2 };
+}
+
 // doubleAndReturnArgs
 // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 
--- script.test.js
+++ script.test.js
@@ -9,3 +9,14 @@ describe("#findMin", () => {
     expect(findMin(20, 14, 98, 7, 67)).toEqual(7);
   });
 });
+
+describe("#mergeObjects", () => {
+  it("should take two objects and create a new object with all the keys and values of the two objects", () => {
+    expect(mergeObjects({ a: 1, b: 2 }, { c: 3, d: 4 })).toEqual({
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+    });
+  });
+});
